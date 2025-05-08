1e45f52fdd893c50ff0ad4a8b7d1fc49b9d61ace
sarthak rode <rodesarthakyt@gmail.com>
2025-05-08 07:15:02 +0000

    const_var_datatypes

---
+++ hehe/hehe.js
@@ -0,0 +1,31 @@
+// const accountId = 457567
+// let accountPasword = "bhaad_me_jao"
+// let accountCity = "paglapur"
+// console.table([accountId, accountPasword , accountCity]);
+
+// alert("3"+"3") // we are using node js not web
+
+// console.log(2313423+24345)
+
+
+//                                   DATATYPES         
+
+let name = "sarthak"
+let age = 17
+let isloggedin = true
+
+// number  => 2 to the power 52
+//bigint 
+// stringh ==>
+// boolern ==> true/ false 
+// null ==> standalone value 
+// undefined ==>
+//symbol==> unique
+
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
