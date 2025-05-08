1c82640006c34a861ab67e60c0259131ebbc3db7
sarthak rode <rodesarthakyt@gmail.com>
2025-05-08 13:26:48 +0000

    operations

---
+++ hehe/03_hehe.js
@@ -0,0 +1,23 @@
+// console.log(2**2);// 2 to the power 2
+
+
+// let str1 = "hello "
+// let str2 = "sarthak"
+
+// let str3 = str1+ str2
+// console.log(str3);
+
+// console.log("1"+2)//12
+// console.log( 1+"2")//12
+// console.log("1"+"1");//11
+// console.log(1 + 1 +"2");//22
+// console.log("1"+ 2 + 2);//112
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
