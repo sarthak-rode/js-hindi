2ef3ac8fb6aee207e2cc8a0f7674510627cb63d0
sarthak rode <rodesarthakyt@gmail.com>
2025-05-08 08:40:15 +0000

    conversion of datatypes

---
+++ hehe/02_hehe.js
@@ -0,0 +1,40 @@
+// let score = 33
+
+// console.log(typeof score); // number 
+
+// let score = "978"
+// // we have to convert in number
+// let ValueInNumber = Number(score)
+
+// console.log(score);
+// console.log(typeof ValueInNumber);//converted into number
+
+
+// let allowed = true
+// let ValueInNumber = Number(allowed)
+
+// console.log(ValueInNumber);// 1
+// console.log(typeof ValueInNumber);//number
+
+
+//// when convert in boolean 
+
+// let IsLoggedIn = 234
+
+// let booleanIsLOggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLOggedIn);
+
+// 1=> true
+//2=>false
+//""==> false 
+// "hitesh"=> true
+//2435345 ==> true
+
+// let SomeNumber = 2345
+// let StrigNumber = String(SomeNumber)
+
+// console.log(StrigNumber);
+
+// console.log(typeof StrigNumber);
+
+
