e33ffd124c42bb769ef25aed5ea53161da517e71
sarthak rode <rodesarthakyt@gmail.com>
2025-05-08 15:35:13 +0000

    comparisons

---
+++ hehe/04_hehe.js
@@ -0,0 +1,24 @@
+//we can find answers logically
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); // true
+// console.log("02" > 1);// true
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);//true 
+
+// console.log(undefined == 0);// always false 
+
+// ====  => strict check , even datatype
+
+// note = avoide these type of comparesions 
+
