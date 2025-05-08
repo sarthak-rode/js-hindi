c2457966ca8cc62f0fd3b9c745f9f63a5fa215aa
sarthak rode <rodesarthakyt@gmail.com>
2025-05-08 16:30:22 +0000

    Detatypes examples

---
+++ hehe/05_hehe.js
@@ -0,0 +1,50 @@
+
+//  PREMATIVE DATATYPES 
+// 1 String
+
+let userName = "sarthak"
+
+// 2 Number
+
+const score = 100
+const scoreValue = 100.32
+
+// 3 Boolean
+
+const IsLoggedIn = true
+const OutsideTemp = null
+
+// 4 null
+// 5 undefined
+
+let userEmail;
+let user2Email = undefined
+
+// 6 BigInt
+
+const bigNumber = 3214536587908909876543456n
+
+// 7 Symbol
+
+const Id = symbol('123')
+const Id2 = sumbol('123') // ids are same but both are unique
+
+
+// Reference (non premative)
+
+// 1 Array
+
+const Heros = ["shaktiman", "nagraj"]
+
+// 2 Object
+
+let myObj = {
+name : "sarthak",
+age : 17,
+}
+//  functions 
+
+const myFunction = function (){}
+
+console.log("hello world");
+
